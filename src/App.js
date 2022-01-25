ed1a04ccbd9f8037c1314e4fb4c77ba4cd97def7
Rob <hello@apro.co>
2022-01-25 15:10:07 +0000

    commit to allow merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, Route, Routes, } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import './App.css';
 import Intro from './Intro'
 import Survey from './Survey'
@@ -10,14 +10,14 @@ export default function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<>
-          <p><Link to="/intro">intro</Link></p>
-          <p><Link to="/survey">survey</Link></p>
-          <p><Link to="/results">results</Link></p>
+        <Route path="/survey-sample" element={<>
+          <p><Link to="/survey-sample/intro">intro</Link></p>
+          <p><Link to="/survey-sample/survey">survey</Link></p>
+          <p><Link to="/survey-sample/results">results</Link></p>
         </>} />
-        <Route path="/intro" element={<Intro />} />
-        <Route path="/survey" element={<Survey />} />
-        <Route path="/results" element={<Results />} />
+        <Route path="/survey-sample/intro" element={<Intro />} />
+        <Route path="/survey-sample/survey" element={<Survey />} />
+        <Route path="/survey-sample/results" element={<Results />} />
       </Routes>
     </div>
   );
