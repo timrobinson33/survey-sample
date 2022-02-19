298531562278f9e030260c8bb5f37929f45e2b30
Rob <hello@apro.co>
2022-02-19 15:41:47 +0000

    Revised header styles

--- src/Intro.js
+++ src/Intro.js
@@ -7,7 +7,7 @@ export default function Intro({ respondent, competencies }) {
       <div class="row m-0 p-0 justify-content-center">
         <div class="col-10 pt-4 d-flex justify-content-center">
           <div class="col-10 justify-content-center">
-            <h1>Introductory page: <span>Insurance band 2 Underwriting</span></h1>
+            <h1>Introductory page: <strong>Insurance band 2 Underwriting</strong></h1>
             <span class="separator"></span>
             <p class="mb-5"><center>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</center></p>
           </div>
