8d32ff70db7beda20ffdcd35f454c1195eb3500c
Rob <hello@apro.co>
2023-09-06 10:22:59 +0100

    Adjust responsive and print styles for results tables

--- src/DutyTable.js
+++ src/DutyTable.js
@@ -26,22 +26,22 @@ export default function DutyTable() {
             <div class="row result-rowinfo">
                 <div class="col-12 mb-3">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I create a culture that allows individuals to learn and develop themselves</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
@@ -49,22 +49,22 @@ export default function DutyTable() {
                 </div>
                 <div class="col-12 mb-3">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I lead strategic changes in working practices and implement organisational plans that maximise the benefits of those changes</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
@@ -79,22 +79,22 @@ export default function DutyTable() {
             <div class="row result-rowinfo">
                 <div class="col-12 mb-3">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I create strategies and systems that reinforce the importance of providing an excellent customer experience</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
@@ -102,22 +102,22 @@ export default function DutyTable() {
                 </div>
                 <div class="col-12 mb-3">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I inspire and champion a culture focused on good customer outcomes</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
@@ -125,22 +125,22 @@ export default function DutyTable() {
                 </div>
                 <div class="col-12 mb-3">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I ensure that the customer is at the heart of decision making and business planning</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
@@ -155,22 +155,22 @@ export default function DutyTable() {
             <div class="row result-rowinfo">
                 <div class="col-12 mb-3 no-printbreak">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I build a culture that encourages open communication and information sharing at all levels</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
@@ -178,22 +178,22 @@ export default function DutyTable() {
                 </div>
                 <div class="col-12 mb-3 no-printbreak">
                     <div class="col-12 mb-3 d-grid results-grid">
-                        <div class="grid-item">
+                        <div class="grid-item statement-item">
                             <p>I build confidence in and gain acceptance of ideas and strategies by relating them to stakeholders' needs and goals</p>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Customer understanding</h5>
                             <span class="col-12 d-block grade-element grade-great"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Price and Value</h5>
                             <span class="col-12 d-block grade-element grade-moderate"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item odd">
                             <h5 class="statement-title">Products and Services</h5>
                             <span class="col-12 d-block grade-element grade-some"></span>
                         </div>
-                        <div class="grid-item">
+                        <div class="grid-item even">
                             <h5 class="statement-title">Customer Support</h5>
                             <span class="col-12 d-block grade-element grade-none"></span>
                         </div>
